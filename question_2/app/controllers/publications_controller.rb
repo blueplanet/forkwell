252a67b133e6e1fc1205968ba3653be14627734c
blueplanet <erguolinge@gmail.com>
2012-12-19 20:29:51 +0900

    Add before_filter

--- question_2/app/controllers/publications_controller.rb
+++ question_2/app/controllers/publications_controller.rb
@@ -1,11 +1,12 @@
 # coding: utf-8
 class PublicationsController < ApplicationController
+  before_filter :find_publication, only: [:show, :add_author, :add_skilltag]
+
   def index
     @publications = Publication.order('created_at desc').page(params[:page]).per(10)
   end
 
   def show
-    @publication = Publication.find params[:id]
   end
 
   def new
@@ -24,8 +25,6 @@ class PublicationsController < ApplicationController
   end
 
   def add_author
-    @publication = Publication.find params[:id]
-
     if @publication.users << current_user
       redirect_to @publication, notice: "#{@publication.title}の著者にあなたを追加しました"
     else
@@ -34,7 +33,6 @@ class PublicationsController < ApplicationController
   end
 
   def add_skilltag
-    @publication = Publication.find params[:id]
     @skilltag = Skilltag.where(name: params[:skilltag]).first_or_create!
 
     if @publication.skilltags << @skilltag
@@ -42,5 +40,10 @@ class PublicationsController < ApplicationController
     else
       redirect_to @publication, notice: "#{@publication.title}のスキルに#{@skilltag.name}を追加できませんでした"
     end
+  end
+
+  private
+  def find_publication
+    @publication = Publication.find params[:id]
   end
 end
